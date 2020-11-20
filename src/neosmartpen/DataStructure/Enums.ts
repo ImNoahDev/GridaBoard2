892a5dd11842e96a1da931df77caa3e76c131dd9
minhee <loitermin@gmail.com>
2020-11-20 13:54:16 +0900

    enums conflict push

--- src/neosmartpen/DataStructure/Enums.ts
+++ src/neosmartpen/DataStructure/Enums.ts
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-/** @enum {string} */
-export enum DeviceTypeEnum {
-  PEN = "pen",
-  ERASER = "eraser",
-  PLAYER = "player",
-  STYLUS = "stylus",
-  FSIR = "fsir",
-  NONE = "none",
-};
-
-
-
-/** @enum {number} */
-export enum PEN_STATE {
-  NONE = 0,
-  PEN_DOWN = 1,
-  PEN_MOVE = 2,
-  PEN_UP = 9,
-
-  HOVER_MOVE = 101,
-};
-
-
-
-/** @enum {string} */
-export enum PenEventName {
-  ON_PEN_DOWN = "pendown",
-  ON_PEN_PAGEINFO = "penpageinfo",
-  ON_PEN_HOVER_PAGEINFO = "hoverpageinfo",
-  ON_PEN_MOVE = "penmove",
-  ON_PEN_UP = "penup",
-  ON_HOVER_MOVE = "hovermove",
-
-  ON_NCODE_ERROR = "error",
-  ON_PW_REQUIRED = "pw_req",
-  ON_CONNECTED = "connected",
-  ON_DISCONNECTED = "disconnected",
-
-  ON_UPGRADE_NEEDED = "fw_up",
-};
-
-
-=======
 /** @enum {string} */
 export enum DeviceTypeEnum {
   PEN = "pen",
@@ -96,4 +52,3 @@ export enum IBrushType {
   FOUNTAINPEN = 5,
 };
 
->>>>>>> e8f9507 (Pen connection fixed)
