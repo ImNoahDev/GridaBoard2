2fe9bb76902d56d997c531ffe362af11c69c5732
minhee <loitermin@gmail.com>
2020-11-25 17:36:57 +0900

    펜 추적 기능 구현중, 버튼 이미지 변경 및 store 사용 부분까지만 구현됨

--- src/components/buttons/TracePointButton.tsx
+++ src/components/buttons/TracePointButton.tsx
@@ -1,10 +1,35 @@
 import React, { Component, useState } from "react";
 import '../../styles/buttons.css';
+import { connect, useSelector, useDispatch } from 'react-redux';
+import { setPointerTracer } from '../../store/reducers/pointerTracer';
+import jQuery from "jquery";
+import { RootState } from '../../store/rootReducer';
 
-export default class TracePointButton extends React.Component {
-  render() {
+let $ = jQuery;
+
+const TracePointButton = () => {
+  const isTrace = useSelector((state:RootState) => state.pointerTracer.isTrace)
+  const dispatch = useDispatch();
+
+  const setEnable = (elem_name: string, sw: boolean) => {
+    var $elem = $(`#${elem_name}`);
+    if (sw) {
+        let $elem = $("#btn_tracepoint").find(".c2");
+        $elem.addClass("checked");
+    } else {
+        let $elem = $("#btn_tracepoint").find(".c2");
+        $elem.removeClass("checked");
+    }
+  }
+  
+  const onTogglePointerTracer = () => {
+    dispatch(setPointerTracer(!isTrace));
+    setEnable("btn_tracepoint", isTrace);
+  }
+
     return (
-      <button id="btn_tracepoint" type="button" className="btn btn-neo btn-neo-vertical" title="Fullscreen">
+      <button id="btn_tracepoint" type="button" className="btn btn-neo btn-neo-vertical" title="Trace Point"
+      onClick = {() => onTogglePointerTracer()}>
           <div className="c2 checked">
               <img src={ require("../../icons/icon_point_d.png") } className="toggle-off normal-image"></img>
               <img src={ require("../../icons/icon_point_p.png") } className="toggle-off hover-image"></img>
@@ -12,6 +37,6 @@ export default class TracePointButton extends React.Component {
               <img src={ require("../../icons/icon_point_p.png") } className="toggle-on hover-image"></img>
           </div>
       </button>
-      );
-  }
+    );
 }
+export default TracePointButton;
