41382a4ae2a1307f7c3bd5fcd06331d447f07db5
ottoro <gonttaro@gmail.com>
2020-12-27 15:12:04 +0900

    최대 확대 크기를 8.0배로

--- src/GridaBoard/GridaDoc.ts
+++ src/GridaBoard/GridaDoc.ts
@@ -1,4 +1,20 @@
+import NeoPdfDocument from "../NcodePrintLib/NeoPdf/NeoPdfDocument";
+import GridaPage from "./GridaPage";
 
 export default class GridaDoc {
+  pages: GridaPage[] = [];
+  
+  addPdfDoc = (pdf: NeoPdfDocument) => {
 
+  }
+
+  addPage = (page: GridaPage) => {
+
+  }
+
+  getPages = () => {
+    const pages: GridaPage[] = [];
+
+    return pages;
+  }
 }
--- src/neosmartpen/renderer/Constants.ts
+++ src/neosmartpen/renderer/Constants.ts
@@ -4,7 +4,7 @@ export const GLOBAL_ZOOM = 2;
 /** 그리다보드 뷰의 확대 최소 값 */
 export const PDFVIEW_ZOOM_MIN = 0.1;
 /** 그리다보드 뷰의 확대 최대 값 */
-export const PDFVIEW_ZOOM_MAX = 20.0;
+export const PDFVIEW_ZOOM_MAX = 8.0;
 
 /** PDF 뷰어의 최대 픽셀 값, byte로는 아래 값*4, 64Mpixel = 256MB */
 export const MAX_RENDERER_PIXELS = 64 * 1024 * 1024;
