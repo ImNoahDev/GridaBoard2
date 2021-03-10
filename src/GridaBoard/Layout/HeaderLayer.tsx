859315d96adb5f94bb6c93198beba4c4ebdffdc9
minihallo-neo <loitermin@neolab.net>
2021-03-10 11:04:32 +0900

    manual calibration에빠진 props 넣어줌

--- src/GridaBoard/Layout/HeaderLayer.tsx
+++ src/GridaBoard/Layout/HeaderLayer.tsx
@@ -165,7 +165,7 @@ const HeaderLayer = () => {
         </Button>
         <LoadGrida />
         <PrintButton targetId={printBtnId} url={pdfUrl} filename={pdfFilename} handlePdfUrl={makePdfUrl}/>
-        <ManualCalibration filename={pdfFilename} printOption={printOption}/>
+        <ManualCalibration filename={pdfFilename} printOption={printOption} handlePdfUrl={makePdfUrl}/>
         <KeyboardArrowDownRoundedIcon style={{float: "right", marginTop: "25px", marginRight: "31px"}}/>
         <p style={{float: "right"}}>구글 이메일</p>
         <ConnectButton onPenLinkChanged={e => onPenLinkChanged(e)} />
