bc74b5678a78a02037c1683e4f3e4e1f9e37230e
minihallo-neo <loitermin@neolab.net>
2021-06-09 11:51:30 +0900

    헤더 레이어 감추기 버튼 수정, NavLayer 테스트 코드 주석 제거

--- src/GridaBoard/Layout/NavLayer.tsx
+++ src/GridaBoard/Layout/NavLayer.tsx
@@ -19,8 +19,9 @@ import BackgroundButton from "../components/buttons/BackgroundButton";
 import FitButton from "../components/buttons/FitButton";
 import PageNumbering from "../components/navbar/PageNumbering";
 import { RootState } from "../store/rootReducer";
-import { makeStyles } from "@material-ui/core";
+import { Collapse, IconButton, makeStyles } from "@material-ui/core";
 import CustomBadge from "../components/CustomElement/CustomBadge"
+import { ArrowDropDown, ArrowDropUp } from "@material-ui/icons";
 
 const useStyle = props => makeStyles(theme => ({
   navStyle : {
@@ -43,7 +44,14 @@ const useStyle = props => makeStyles(theme => ({
       position: "relative",
       left: "-6%",
     }
-  }
+  },
+  headerViewBtn : {
+    zIndex: 0,
+    height:"24px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 }));
 
 const printBtnId = "printTestButton";
@@ -55,10 +63,12 @@ const getNoteInfo = (event) => {
   // note_info.getNoteInfo({});
 };
 
-const NavLayer = () => {
+const NavLayer = (props) => {
   const [num_pens, setNumPens] = useState(0);
   const [mapViewDetail, setMapViewDetail] = useState(0);
   const [docViewDetail, setDocViewDetail] = useState(0);
+  const [isCollapsed, setCollapsed] = useState(false);
+
   const brZoom = useSelector((state: RootState) => state.ui.browser.zoom);
   const classes = useStyle({brZoom:brZoom})();
   const badgeInVisible = !useSelector((state: RootState) => state.ui.shotcut.show);
@@ -121,146 +131,45 @@ const NavLayer = () => {
     docJson.pages.push(obj);
   }
 
+  const HeaderController = (props)=>{
+    return (
+    <div className={classes.headerViewBtn} onClick={props.hideHeader}> 
+      <IconButton aria-label="open drawer" onClick={() => { setCollapsed(prev => !prev) }} >
+        {!isCollapsed? (<ArrowDropUp />) : (<ArrowDropDown />)}
+      </IconButton>
+    </div>);
+  }
 
   return (
     <div className={classes.navStyle}>
-      <div>
-        <PenTypeButton />
+        <div>
+          <PenTypeButton />
 
-        <CustomBadge badgeContent={`1~0`}>
-          <ColorButtons />
-        </CustomBadge>
-        
-        <CustomBadge badgeContent={`Z~B`}>
-          <ThicknessButton />
-        </CustomBadge>
+          <CustomBadge badgeContent={`1~0`}>
+            <ColorButtons />
+          </CustomBadge>
+          
+          <CustomBadge badgeContent={`Z~B`}>
+            <ThicknessButton />
+          </CustomBadge>
 
-        <CustomBadge badgeContent={`T`}>
-          <TracePointButton />
-        </CustomBadge>
-      </div>
+          <CustomBadge badgeContent={`T`}>
+            <TracePointButton />
+          </CustomBadge>
+        </div>
 
-      <div>
-        <PageNumbering />
-      </div>
+        <div>
+          <PageNumbering />
+        </div>
 
-      <div>
-        <BackgroundButton />
-
-        <FitButton />
-      </div>
-
-
-      {/* 매핑 정보 살펴보기 */}
-      {/* <div style={{ fontSize: "20px", fontWeight: "bold" }}>
-        <Button variant="contained" color="primary"
-          onClick={(event) => setMapViewDetail((mapViewDetail + 1) % 3)} >
-          <Box fontSize={14} fontWeight="fontWeightBold" >매핑 테이블</Box>
-        </Button>
-      </div>
-
-      { mapViewDetail
-        ? <Paper style={{ position: "absolute", top: 100, minWidth: 800, maxHeight: 800, overflow: 'auto' }}>
-          <ReactJson src={mapJson}
-            displayDataTypes={false}
-            name={"MappingStorage._data"} collapsed={4} theme="monokai" />
-        </Paper>
-        : ""}
-
-      <div style={{ flex: 1 }}> </div> */}
-      {/* GridaDoc 내부 */}
-      {/* <div style={{ fontSize: "20px", fontWeight: "bold" }}>
-        <Button variant="contained" color="primary"
-          onClick={(event) => setDocViewDetail((docViewDetail + 1) % 2)} >
-          <Box fontSize={14} fontWeight="fontWeightBold" >GridaDoc</Box>
-        </Button>
-      </div>
-
-      { docViewDetail
-        ? <Paper style={{ position: "absolute", top: 100, minWidth: 800, maxHeight: 800, overflow: 'auto' }}>
-          <ReactJson src={docJson}
-            displayDataTypes={false}
-            name={"MappingStorage._data"} collapsed={3} theme="monokai" />
-        </Paper>
-        : ""}
-
-      <div style={{ flex: 1 }}> </div> */}
-      {/* 공책 정보 가져오기 테스트 버튼 */}
-      {/* <div style={{ flex: 1 }}> </div> */}
-
-      {/* 공책 정보 가져오기 테스트 버튼 */}
-      {/* <div style={{ fontSize: "20px", fontWeight: "bold" }}>
-        <Button variant="outlined" color="primary" onClick={(event) => getNoteInfo(event)} >
-          <Box fontSize={14} fontWeight="fontWeightBold" >공책 정보 받아오기(현재 실패)</Box>
-        </Button>
-      </div>
-      <div style={{ flex: 1 }}> </div> */}
-
-      {/* 이미지 버튼 테스트 */}
-      {/* <NeoImage src="/icons/icon_trash_n.png" /> */}
-
-      {/* 인쇄 테스트 버튼 */}
-      {/* <div style={{ fontSize: "20px", fontWeight: "bold" }}>
-        <PrintPdfButton variant="contained" color="primary"
-          id={printBtnId}
-          url={pdfUrl}
-          filename={pdfFilename}
-          printOption={printOption}
-          reportProgress={undefined} printOptionCallback={undefined}>
-          <Box fontSize={14} fontWeight="fontWeightBold" >인쇄 시험 (인쇄 옵션 창을 띄울것)</Box>
-        </PrintPdfButton>
-      </div>
-      <div style={{ flex: 1 }}> </div> */}
-
-
-      {/* High-speed 인쇄 테스트 버튼 */}
-      {/* <div style={{ fontSize: "20px", fontWeight: "bold" }}>
-        <PrintNcodedPdfButton variant="contained" color="primary"
-          id={printBtnId}
-          url={pdfUrl} filename={pdfFilename}
-          handkeTurnOnAppShortCutKey={turnOnGlobalKeyShortCut}
-          // printOption={printOption}
-          reportProgress={undefined} printOptionCallback={undefined}>
-          <Box fontSize={14} fontWeight="fontWeightBold" >Ncode PDF</Box>
-        </PrintNcodedPdfButton>
-      </div>
-      <div style={{ flex: 1 }}> </div> */}
-
-
-      {/* 칼리브레이션 */}
-      {/* <CalibrationButton url={pdfUrl} filename={pdfFilename} printOption={printOption} cancelCallback={undefined}>
-        <Box fontSize={14} fontWeight="fontWeightBold" >Calibration</Box>
-      </CalibrationButton> */}
-
-
-      {/* 매핑 정보 지우기 버튼 */}
-      {/* <div style={{ fontSize: "20px", fontWeight: "bold" }}>
-        <ClearLocalMappingButton variant="contained" color="secondary" >
-          <Box fontSize={14} fontWeight="fontWeightBold" >매핑정보 지우기</Box>
-        </ClearLocalMappingButton>
-      </div>
-      <div style={{ flex: 1 }}> </div> */}
-
-      {/* [신] 인쇄 옵션 다이얼로그 테스트 버튼 */}
-      {/* <div style={{ fontSize: "14px" }}>
-        <OptionDialogButton
-          printOption={printOption}
-          handkeTurnOnAppShortCutKey={turnOnGlobalKeyShortCut}
-        >
-
-          <Box fontSize={14} fontWeight="fontWeightBold" >신규, 인쇄옵션</Box>
-        </OptionDialogButton>
-      </div>
-
-      <div style={{ flex: 1 }}> </div> */}
-
-      {/* 구글 업로드 테스트 버튼 */}
-      {/* <Upload />
-      <div style={{ flex: 1 }}> </div>
-      <GoogleBtn />
-      <div style={{ flex: 11 }}> </div> */}
+        <div>
+          <BackgroundButton />
+          <FitButton />
+          <HeaderController {...props} /> 
+        </div>
     </div>
   );
 }
 
 export default NavLayer;
+
