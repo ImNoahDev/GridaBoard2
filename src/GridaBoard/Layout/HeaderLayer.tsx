2b34c0b38db628fdcd452b3129e690fbb86d7f2d
minihallo-neo <loitermin@neolab.net>
2021-07-07 18:06:01 +0900

    head layer 메뉴 버튼 ui 변경

--- src/GridaBoard/Layout/HeaderLayer.tsx
+++ src/GridaBoard/Layout/HeaderLayer.tsx
@@ -1,189 +1,192 @@
-import React, { useState } from "react";
-import { Button, makeStyles, ClickAwayListener, IconButton } from "@material-ui/core";
+import React, { useState } from 'react';
+import { Button, makeStyles, ClickAwayListener, IconButton } from '@material-ui/core';
 // import LoadGrida from "../Load/LoadGrida";
-import ConvertFileLoad from "../Load/ConvertFileLoad";
-import GridaDoc from "../GridaDoc";
+import ConvertFileLoad from '../Load/ConvertFileLoad';
+import GridaDoc from '../GridaDoc';
 import { PDFDocument } from 'pdf-lib';
-import ConnectButton from "../components/buttons/ConnectButton";
-import GridaApp from "../GridaApp";
+import ConnectButton from '../components/buttons/ConnectButton';
+import GridaApp from '../GridaApp';
 // import ManualCalibration from "../components/navbar/ManualCalibration";
-import { g_defaultPrintOption, PrintNcodedPdfButton } from "nl-lib/ncodepod";
-import SavePdfDialog from "../Save/SavePdfDialog";
+import { g_defaultPrintOption, PrintNcodedPdfButton } from 'nl-lib/ncodepod';
+import SavePdfDialog from '../Save/SavePdfDialog';
 // import { FileBrowserButton } from "nl-lib/common/neopdf";
-import { IFileBrowserReturn } from "nl-lib/common/structures";
+import { IFileBrowserReturn } from 'nl-lib/common/structures';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store/rootReducer';
-import { turnOnGlobalKeyShortCut } from "../GlobalFunctions";
-import getText from "../language/language";
+import { turnOnGlobalKeyShortCut } from '../GlobalFunctions';
+import getText from '../language/language';
 import { CalibrationButton } from 'nl-lib/ncodepod';
-import CustomBadge from "../components/CustomElement/CustomBadge";
-import LogoSvg from "../logo.svg";
-import TestButton from "../components/buttons/TestButton";
-import { useHistory } from "react-router";
-import SimpleTooltip from "../components/SimpleTooltip";
-import { KeyboardArrowDown } from "@material-ui/icons";
+import CustomBadge from '../components/CustomElement/CustomBadge';
+import LogoSvg from '../logo.svg';
+import TestButton from '../components/buttons/TestButton';
+import { useHistory } from 'react-router';
+import SimpleTooltip from '../components/SimpleTooltip';
+import { KeyboardArrowDown } from '@material-ui/icons';
 import { auth } from 'GridaBoard/util/firebase_config';
-import ProfileButton from "../components/buttons/ProfileButton";
-import { showAlert } from "../store/reducers/listReducer";
+import ProfileButton from '../components/buttons/ProfileButton';
+import { showAlert } from '../store/reducers/listReducer';
 
-const useStyles = props => makeStyles((theme) => ({
-  dropdownBtn : {
-    width: "200px",
-    height: "40px",
-    padding: "4px 12px",
-    display: "flex",
-    justifyContent: "left",
-    "&:hover" : {
-      background : theme.custom.icon.blue[3],
-      color: theme.palette.action.hover
-    }
-  },
-  buttonStyle: {
-    padding: 0,
-    minWidth: "0px",
-    minHeight: "0px"
-  },
-  calibration: {
-    background: theme.custom.white[50],
-    border: "1px solid #CFCFCF",
-    boxSizing: "border-box",
-    borderRadius: "4px",
-    fontSize: "12px",
-    fontFamily: "Roboto",
-    padding: "8px",
-    textTransform: 'none'
-  },
-  buttonFontStyle: {
-    minWidth: "0px",
-    fontFamily: "Roboto",
-    fontStyle: "normal",
-    fontWeight: 400,
-    lineHeight: "16.41px",
-    fontSize: "14px",
-    textAlign: "right",
-    letterSpacing: "0.25px",
-    color: "#666666",
-    "&:hover": {
-      color: theme.palette.action.hover,
-      fontWeight: 700
-    }
-  },
-  saveDropdownStyle: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "flex-start",
-    padding: "8px",
-    position: "absolute",
-    background: theme.custom.icon.mono[4],
-    boxShadow: theme.custom.shadows[0],
-    borderRadius: "12px",
-    zIndex: 10000,
-    marginTop: "21px",
-    marginLeft: "-1px"
-  },
-  headerButtons: {
-    display: "flex",
-    flexDirection: "column",
-    flexFlow: "column"
-  },
-  headerStyle: {
-    display: "flex",
-    zIndex: 2,
-    position: "relative",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    flexWrap: "wrap",
-    height: "72px",
-    zoom: 1 / props.brZoom,
-    backdropFilter: "blur(4px)",
-    "& > div": {
-      display: "flex",
-      flexDirection: "row",
-      justifyContent: "flex-start",
-      alignItems: "center",
-      margin: "0 24px"
+const useStyles = props =>
+  makeStyles(theme => ({
+    dropdownBtn: {
+      width: '200px',
+      height: '40px',
+      padding: '4px 12px',
+      display: 'flex',
+      justifyContent: 'left',
+      '&:hover': {
+        background: theme.custom.icon.blue[3],
+        color: theme.palette.action.hover,
+      },
     },
-    "& > div > div": { //캘리브 제외하고 전부
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "flex-start",
-      // alignItems: "center",
+    buttonStyle: {
+      padding: 0,
+      minWidth: '0px',
+      minHeight: '0px',
     },
-    "& > div:first-child > div": { //앞에 애들 전부
-      marginLeft: "24px",
-      marginRight: "24px",
-      padding: "10px",
+    calibration: {
+      background: theme.custom.white[50],
+      border: '1px solid #CFCFCF',
+      boxSizing: 'border-box',
+      borderRadius: '4px',
+      fontSize: '12px',
+      fontFamily: 'Roboto',
+      padding: '8px',
+      textTransform: 'none',
     },
-    "& > div:last-child > div": { //연결버튼 + 구버전버튼
-      marginLeft: "16px",
+    buttonFontStyle: {
+      minWidth: '0px',
+      fontFamily: 'Roboto',
+      fontStyle: 'normal',
+      fontWeight: 400,
+      lineHeight: '16.41px',
+      fontSize: '14px',
+      textAlign: 'right',
+      letterSpacing: '0.25px',
+      color: '#666666',
+      '&:hover': {
+        color: theme.palette.action.hover,
+        fontWeight: 700,
+      },
     },
-    "& > div > div > div": {
-      display: "flex",
-      flexDirection: "row",
+    saveDropdownStyle: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+      padding: '8px',
+      position: 'absolute',
+      background: theme.custom.icon.mono[4],
+      boxShadow: theme.custom.shadows[0],
+      borderRadius: '12px',
+      zIndex: 10000,
+      marginTop: '21px',
+      marginLeft: '-1px',
     },
-    "& > div > div > div:first-child": { //문서 제목
-      fontWeight: 700,
-      fontSize: "18px",
+    headerButtons: {
+      display: 'flex',
+      flexDirection: 'column',
+      flexFlow: 'column',
     },
-    "& > div > div > div:last-child > div": { //파일 수정 보기 기타등등
-      marginRight: "24px",
+    headerStyle: {
+      display: 'flex',
+      zIndex: 2,
+      position: 'relative',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      flexWrap: 'wrap',
+      height: '72px',
+      zoom: 1 / props.brZoom,
+      backdropFilter: 'blur(4px)',
+      '& > div': {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        margin: '0 24px',
+      },
+      '& > div > div': {
+        //캘리브 제외하고 전부
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        // alignItems: "center",
+      },
+      '& > div:first-child > div': {
+        //앞에 애들 전부
+        marginLeft: '24px',
+        marginRight: '24px',
+        padding: '10px',
+      },
+      '& > div:last-child > div': {
+        //연결버튼 + 구버전버튼
+        marginLeft: '16px',
+      },
+      '& > div > div > div': {
+        display: 'flex',
+        flexDirection: 'row',
+      },
+      '& > div > div > div:first-child': {
+        //문서 제목
+        fontWeight: 700,
+        fontSize: '18px',
+      },
+      '& > div > div > div:last-child > div': {
+        //파일 수정 보기 기타등등
+        marginRight: '24px',
+      },
     },
-  },
-  headerLineV: {
-    width: "1px",
-    height: "15px",
-    background: theme.custom.icon.mono[3]
-  },
-  changeUrlStyle: {
-    justifyContent: "center",
-    background: theme.custom.white[50],
-    border: "1px solid #CFCFCF",
-    boxSizing: "border-box",
-    borderRadius: "4px",
-    overflow: "hidden",
-    textOverflow: "ellipsis", 
-    whiteSpace: "nowrap",
-    padding: "3px",
-    "& > span": {
-      fontSize: "12px",
-      lineHeight: "14px",
-      padding: "5px",
-      fontWeight: 500
-    }
-  },
-  imgStyle: {
-    borderRadius: "8px"
-  },
-  badge: {
-    background: theme.custom.icon.mono[1],
-    color: theme.custom.icon.mono[4]
-  }
-}));
+    headerLineV: {
+      width: '1px',
+      height: '15px',
+      background: theme.custom.icon.mono[3],
+    },
+    changeUrlStyle: {
+      justifyContent: 'center',
+      background: theme.custom.white[50],
+      border: '1px solid #CFCFCF',
+      boxSizing: 'border-box',
+      borderRadius: '4px',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+      whiteSpace: 'nowrap',
+      padding: '3px',
+      '& > span': {
+        fontSize: '12px',
+        lineHeight: '14px',
+        padding: '5px',
+        fontWeight: 500,
+      },
+    },
+    imgStyle: {
+      borderRadius: '8px',
+    },
+    badge: {
+      background: theme.custom.icon.mono[1],
+      color: theme.custom.icon.mono[4],
+    },
+  }));
 
-
-const printBtnId = "printTestButton";
+const printBtnId = 'printTestButton';
 const printOption = g_defaultPrintOption;
 
 interface Props {
-  handlePdfOpen: (event: IFileBrowserReturn) => void,
-  handlePenLogWindow: () => void,
-  hidden: boolean
+  handlePdfOpen: (event: IFileBrowserReturn) => void;
+  handlePenLogWindow: () => void;
+  hidden: boolean;
 }
 
 const HeaderLayer = (props: Props) => {
-
   const { handlePdfOpen, ...rest } = props;
 
   const [pdfUrl, setPdfUrl] = useState(undefined as string);
   const [pdfFilename, setPdfFilename] = useState(undefined as string);
+
   const docName = useSelector((state: RootState) => state.docConfig.docName);
 
-  const history = useHistory();
-
   function fileOpenHandler() {
-    const input = document.querySelector("#fileForconvert") as HTMLInputElement;
-    input.value = "";
+    const input = document.querySelector('#fileForconvert') as HTMLInputElement;
+    input.value = '';
     input.click();
   }
 
@@ -192,7 +195,8 @@ const HeaderLayer = (props: Props) => {
     const docPages = doc.pages;
     let isPdfEdited = false;
 
-    let pdfUrl, pdfDoc = undefined;
+    let pdfUrl,
+      pdfDoc = undefined;
 
     for (const page of docPages) {
       if (page.pdf === undefined) {
@@ -209,8 +213,7 @@ const HeaderLayer = (props: Props) => {
           pdfPage.setWidth(pdfPage.getHeight());
           pdfPage.setHeight(tmpWidth);
         }
-      }
-      else {
+      } else {
         //pdf인 경우
         if (pdfUrl !== page.pdf.url) {
           pdfUrl = page.pdf.url;
@@ -239,7 +242,6 @@ const HeaderLayer = (props: Props) => {
 
             const copiedPages = await pdfDoc.copyPages(pdfDocSrc, totalPageArr);
 
-
             for (const copiedPage of copiedPages) {
               await pdfDoc.addPage(copiedPage);
             }
@@ -262,13 +264,12 @@ const HeaderLayer = (props: Props) => {
 
     const url = await URL.createObjectURL(blob);
     return url;
-  }
+  };
 
   const onPenLinkChanged = e => {
     const app = GridaApp.getInstance();
     app.onPenLinkChanged(e);
-  }
-
+  };
 
   const activePageNo_store = useSelector((state: RootState) => state.activePage.activePageNo);
 
@@ -278,8 +279,8 @@ const HeaderLayer = (props: Props) => {
   const classes = useStyles({ brZoom })();
 
   const HeaderLine = () => {
-    return (<div className={classes.headerLineV} />);
-  }
+    return <div className={classes.headerLineV} />;
+  };
 
   console.log(`browser zoom level changed = ${brZoom}`);
 
@@ -288,88 +289,129 @@ const HeaderLayer = (props: Props) => {
     disabled = false;
   }
 
-
   const [isSaveOpen, setIsSaveOpen] = useState(false);
+  const [isExportOpen, setIsExportOpen] = useState(false);
 
-  function handleClickSave() {
-    setIsSaveOpen((prev) => !prev);
-  }
-  function handleClickSaveAway() {
+  const handleClickSave = () => {
+    setIsSaveOpen(true);
+  };
+
+  const handleClickExport = () => {
+    setIsExportOpen(true);
+  };
+
+  const handleClickSaveAway = () => {
     setIsSaveOpen(false);
-  }
+  };
+
+  const handleClickExportAway = () => {
+    setIsExportOpen(false);
+  };
 
   const handlePenLogWindow = () => {
     props.handlePenLogWindow();
-  }
+  };
 
   const toBoardList = async () => {
     showAlert({
-      type:"toBoardList",
+      type: 'toBoardList',
       selected: null,
-      sub: null
+      sub: null,
     });
-  }
+  };
 
   const [debugOpen, setDebugOpen] = useState(false);
 
   return (
     <React.Fragment>
       <div id="header" className={`${classes.headerStyle}`}>
-        <div >
-            <SimpleTooltip title="그리다보드 홈">
-              <IconButton><img src={LogoSvg} className={classes.imgStyle} onClick={toBoardList}></img></IconButton>
-            </SimpleTooltip>
-          <div>  
+        <div>
+          <SimpleTooltip title="그리다보드 홈">
+            <IconButton>
+              <img src={LogoSvg} className={classes.imgStyle} onClick={toBoardList}></img>
+            </IconButton>
+          </SimpleTooltip>
+          <div>
             <div>{docName}</div> {/* & > div > div > div:first-child */}
-            <div> {/* & > div > div > div:last-child */}
+            <div>
+              {' '}
+              {/* & > div > div > div:last-child */}
               <ClickAwayListener onClickAway={handleClickSaveAway}>
                 <div>
                   <CustomBadge badgeContent={`S`}>
-                    <Button className={`${classes.buttonStyle} ${classes.buttonFontStyle} saveButton`} onClick={handleClickSave} disabled={disabled}>
-                      {getText("save_file")}
+                    <Button
+                      className={`${classes.buttonStyle} ${classes.buttonFontStyle} saveButton`}
+                      onClick={handleClickSave}
+                      disabled={disabled}>
+                      {getText('save_file')}
                     </Button>
                   </CustomBadge>
                   {isSaveOpen ? (
-                    <div className={`${classes.saveDropdownStyle}`} >
-                      <SavePdfDialog saveType="pdf" />
-                      <SavePdfDialog saveType="grida" />
+                    <div className={`${classes.saveDropdownStyle}`}>
                       <SavePdfDialog saveType="saveAs" />
                       <SavePdfDialog saveType="overwrite" />
                     </div>
                   ) : null}
                 </div>
               </ClickAwayListener>
+              <ClickAwayListener onClickAway={handleClickExportAway}>
+                <div>
+                  <CustomBadge badgeContent={`S`}>
+                    <Button
+                      className={`${classes.buttonStyle} ${classes.buttonFontStyle} saveButton`}
+                      onClick={handleClickExport}
+                      disabled={disabled}>
+                      내보내기
+                    </Button>
+                  </CustomBadge>
+                  {isExportOpen ? (
+                    <div className={`${classes.saveDropdownStyle}`}>
+                      <SavePdfDialog saveType="pdf" />
+                      <SavePdfDialog saveType="grida" />
+                    </div>
+                  ) : null}
+                </div>
+              </ClickAwayListener>
               <div>
                 <CustomBadge badgeContent={`Ctrl-O`}>
-                  <Button id="loadFileButton" className={`loadDropDown ${classes.buttonStyle} ${classes.buttonFontStyle}`}
-                  onClick={fileOpenHandler}>
-                    {getText("load_file")}
+                  <Button
+                    id="loadFileButton"
+                    className={`loadDropDown ${classes.buttonStyle} ${classes.buttonFontStyle}`}
+                    onClick={fileOpenHandler}>
+                    {getText('load_file')}
                     <ConvertFileLoad handlePdfOpen={handlePdfOpen} />
                   </Button>
                 </CustomBadge>
               </div>
               <div>
                 <CustomBadge badgeContent={`P`}>
-                  <PrintNcodedPdfButton id="printBtn"
+                  <PrintNcodedPdfButton
+                    id="prin tBtn"
                     className={` ${classes.buttonStyle}  ${classes.buttonFontStyle}`}
                     handkeTurnOnAppShortCutKey={turnOnGlobalKeyShortCut}
-
-                    url={pdfUrl} filename={pdfFilename} handlePdfUrl={makePdfUrl} disabled={disabled} />
+                    url={pdfUrl}
+                    filename={pdfFilename}
+                    handlePdfUrl={makePdfUrl}
+                    disabled={disabled}
+                  />
                 </CustomBadge>
               </div>
+              <div>
+                <CalibrationButton
+                  className={`${classes.buttonStyle}  ${classes.buttonFontStyle}`}
+                  filename={pdfFilename}
+                  handlePdfUrl={makePdfUrl}
+                />
+              </div>
             </div>
-            
           </div>
 
- 
-          <CalibrationButton className={`${classes.buttonStyle}  ${classes.calibration}`} filename={pdfFilename} handlePdfUrl={makePdfUrl} />
-
           <div>
-            <TestButton className={`${classes.buttonStyle}`} onClick={(e) => handlePenLogWindow()} hidden={props.hidden}/>
+            <TestButton className={`${classes.buttonStyle}`} onClick={e => handlePenLogWindow()} hidden={props.hidden} />
           </div>
         </div>
 
-        <div >
+        <div>
           <div>
             <ConnectButton className={`${classes.buttonStyle}`} onPenLinkChanged={e => onPenLinkChanged(e)} />
           </div>
@@ -377,9 +419,8 @@ const HeaderLayer = (props: Props) => {
           
           <InformationBtn /> */}
           <HeaderLine />
-          
-          <ProfileButton/>
-          
+
+          <ProfileButton />
 
           {/* <div>
             <Button href="https://gridaboard-v1-30576.web.app/" className={`${classes.buttonStyle} ${classes.changeUrlStyle}`}>
@@ -390,11 +431,9 @@ const HeaderLayer = (props: Props) => {
           <KeyboardArrowDownRoundedIcon /> */}
         </div>
       </div>
-      <div style={{ height: "1px", background: "rgba(255,255,255,1)", zoom: 1 / brZoom }}>
-     
-      </div>
+      <div style={{ height: '1px', background: 'rgba(255,255,255,1)', zoom: 1 / brZoom }}></div>
     </React.Fragment>
   );
-}
+};
 
 export default HeaderLayer;
